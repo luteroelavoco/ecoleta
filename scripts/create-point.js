3d13c373b15488d2d5838b4bc934839678abf90b
cientista1 <40244138+cientista1@users.noreply.github.com>
2020-06-02 23:42:51 -0300

    Create-point

---
+++ scripts/create-point.js
@@ -0,0 +1,38 @@
+ 
+
+function PopulateUFs(){
+    const ufSelect = document.querySelector("select[name=uf]");
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+        .then((res) => res.json())
+        .then((states) => {
+            for(state of states){
+                ufSelect.innerHTML += `<option value="${state.id}"> ${state.nome} </option>`;
+            }
+
+        })
+}
+
+function getCities(event){
+    const citySelect = document.querySelector("select[name=city]");
+    const stateInput = document.querySelector("input[name=state]");
+    const ufValue = event.target.value;
+    const index = event.target.selectedIndex;
+    stateInput.value = event.target.options[index].text;
+    console.log(stateInput.value);
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios?orderBy=nome`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((cities) => {
+            for(city of cities){
+                citySelect.innerHTML += `<option value="${city.id}"> ${city.nome} </option>`;
+            }
+            citySelect.disabled = false
+
+        })
+}
+
+PopulateUFs()
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change",getCities)
